b0d71329dbf64c8fe36ebb0cc0033eb4b23500c0
afolabiibrahimtech <afolabiibrahim.dream0@gmail.com>
2026-09-20 10:04:34 -0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FieldServiceApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
